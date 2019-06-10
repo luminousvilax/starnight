d48aa36ec49a11a8836273c72c4cfcdfd1fcb19a
luminousvilax <47686069+luminousvilax@users.noreply.github.com>
2019-06-10 20:10:27 +0800

    Update v2ray_setup.sh

--- v2ray_setup.sh
+++ v2ray_setup.sh
@@ -3,6 +3,6 @@ yum -y install wget net-tools vim
 mkdir /root/v2ray
 cd /root/v2ray
 wget https://install.direct/go.sh
-chmod +x /root/go.sh
+chmod +x ./go.sh
 ./go.sh
 cp /etc/v2ray/config.json /etc/v2ray/config.json.origin
